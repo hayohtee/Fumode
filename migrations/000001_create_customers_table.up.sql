5f24a46d2fd6a38e28cc39953787ade77887e653
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-13 17:33:45 +0100

    Update 000001_create_customers_table.up.sql

--- migrations/000001_create_customers_table.up.sql
+++ migrations/000001_create_customers_table.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS customers
 (
     customer_id  BIGSERIAL PRIMARY KEY,
-    name         TEXT          NOT NUlL,
-    email        citext UNIQUE NOT NULL,
+    name         TEXT                        NOT NUlL,
+    email        citext UNIQUE               NOT NULL,
     password     BYTEA,
     address      TEXT,
     phone_number VARCHAR(100),
     created_at   TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW(),
-    version      int           NOT NULL DEFAULT 1
+    version      INTEGER                     NOT NULL DEFAULT 1
 );
