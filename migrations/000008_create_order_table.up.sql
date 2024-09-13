dc5a14feac35405e2372ebfc7d86b4f49ec7d7de
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-13 21:50:45 +0100

    Update order table

--- migrations/000008_create_order_table.up.sql
+++ migrations/000008_create_order_table.up.sql
@@ -1,10 +1,9 @@
 CREATE TABLE IF NOT EXISTS "order"
 (
-    order_id    BIGSERIAL,
+    order_id    BIGSERIAL PRIMARY KEY,
     order_date  TIMESTAMP(0) WITH TIME ZONE,
     total_price DECIMAL(10, 2) NOT NULL,
-    customer_id BIGINT         NOT NULL REFERENCES customer (customer_id) ON DELETE CASCADE,
+    customer_id BIGINT REFERENCES customer (customer_id),
     payment_id  BIGINT         NOT NULL REFERENCES payment (payment_id) ON DELETE CASCADE,
-    shipment_id BIGINT         NOT NULL REFERENCES shipment (shipment_id) ON DELETE CASCADE,
-    PRIMARY KEY (order_id, customer_id, payment_id, shipment_id)
+    shipment_id BIGINT         NOT NULL REFERENCES shipment (shipment_id) ON DELETE CASCADE
 );
