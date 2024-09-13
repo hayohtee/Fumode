2475e44c53998b5835a8a7971bdb589f80c7f5eb
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-13 22:09:50 +0100

    Update cart table

--- migrations/000005_create_cart_table.up.sql
+++ migrations/000005_create_cart_table.up.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS cart
 (
     cart_id      BIGSERIAL NOT NULL,
     customer_id  BIGINT    NOT NULL REFERENCES customer (customer_id) ON DELETE CASCADE,
-    furniture_id BIGINT    REFERENCES furniture (furniture_id) ON DELETE CASCADE,
+    furniture_id BIGINT    NOT NULL REFERENCES furniture (furniture_id) ON DELETE CASCADE,
     quantity     INTEGER   NOT NULL,
     PRIMARY KEY (cart_id, customer_id)
 );
