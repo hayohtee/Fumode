3046a12dc41db0c44cea7c3c23a77736c7bc44e6
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-13 22:18:14 +0100

    Update order_item

--- migrations/000009_create_order_item_table.up.sql
+++ migrations/000009_create_order_item_table.up.sql
@@ -5,5 +5,5 @@ CREATE TABLE IF NOT EXISTS order_item
     price             DECIMAL(10, 2) NOT NULL,
     furniture_id      BIGINT REFERENCES furniture (furniture_id),
     customer_order_id BIGINT         NOT NULL REFERENCES customer_order (customer_order_id) ON DELETE CASCADE,
-    PRIMARY KEY (order_item_id, order_id)
+    PRIMARY KEY (order_item_id, customer_order_id)
 );
