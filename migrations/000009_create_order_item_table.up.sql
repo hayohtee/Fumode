3bbdbdedec4b0525768b9f23ba4a2c958a824b9b
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-13 21:56:01 +0100

    Add primary key to order_item table

--- migrations/000009_create_order_item_table.up.sql
+++ migrations/000009_create_order_item_table.up.sql
@@ -4,5 +4,6 @@ CREATE TABLE IF NOT EXISTS order_item
     quantity      INT            NOT NULL,
     price         DECIMAL(10, 2) NOT NULL,
     furniture_id  BIGINT REFERENCES furniture (furniture_id),
-    order_id      BIGINT         NOT NULL REFERENCES order (order_id) ON DELETE CASCADE
+    order_id      BIGINT         NOT NULL REFERENCES order (order_id) ON DELETE CASCADE,
+    PRIMARY KEY (order_item_id, order_id)
 );
