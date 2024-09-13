41b013596d1102f3f99c6c42c968aaf7dc2a2c8d
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-13 22:06:23 +0100

    Update payment table

--- migrations/000006_create_payment_table.up.sql
+++ migrations/000006_create_payment_table.up.sql
@@ -1,9 +1,8 @@
 CREATE TABLE IF NOT EXISTS payment
 (
-    payment_id     BIGSERIAL,
+    payment_id     BIGSERIAL PRIMARY KEY,
     payment_date   TIMESTAMP(0) WITH TIME ZONE,
     payment_method VARCHAR(100),
     amount         DECIMAL(10, 2),
-    customer_id    BIGINT NOT NULL REFERENCES customer (customer_id),
-    PRIMARY KEY (payment_id, customer_id)
+    customer_id    BIGINT REFERENCES customer (customer_id)
 );
