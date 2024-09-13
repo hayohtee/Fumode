6a5675145293763d2381b9ecd2bb1b92532d851b
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-13 20:08:02 +0100

    Add version attribute to categories table

--- migrations/000002_create_categories_table.up.sql
+++ migrations/000002_create_categories_table.up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS categories
 (
     category_id BIGSERIAL PRIMARY KEY,
-    name        VARCHAR(100) NOT NULL
+    name        VARCHAR(100) NOT NULL,
+    version     INTEGER      NOT NULL DEFAULT 1
 );
