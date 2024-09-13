06abf490bced9bd67afcd5b3babb259fac0e4398
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-13 21:34:44 +0100

    Add ON DELETE option to cart table

--- migrations/000005_create_cart_table.up.sql
+++ migrations/000005_create_cart_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS cart
 (
     cart_id      BIGSERIAL NOT NULL,
-    customer_id  BIGINT    NOT NULL REFERENCES customer (customer_id),
-    furniture_id BIGINT    NOT NULL REFERENCES furniture (furniture_id),
+    customer_id  BIGINT    NOT NULL REFERENCES customer (customer_id) ON DELETE CASCADE,
+    furniture_id BIGINT    NOT NULL REFERENCES furniture (furniture_id) ON DELETE CASCADE,
     quantity     INTEGER   NOT NULL,
     PRIMARY KEY (cart_id, customer_id, furniture_id)
 );
