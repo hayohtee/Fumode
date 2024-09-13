d11119d2626c4300bf03d1730cf7dd355ca64ad0
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-13 21:22:11 +0100

    Add primary key constraint to cart table

--- migrations/000005_create_cart_table.up.sql
+++ migrations/000005_create_cart_table.up.sql
@@ -3,5 +3,6 @@ CREATE TABLE IF NOT EXISTS cart
     cart_id      BIGSERIAL NOT NULL,
     customer_id  BIGINT    NOT NULL REFERENCES customer (customer_id),
     furniture_id BIGINT    NOT NULL REFERENCES furniture (furniture_id),
-    quantity     INTEGER   NOT NULL
+    quantity     INTEGER   NOT NULL,
+    PRIMARY KEY (cart_id, customer_id, furniture_id)
 );
