2ae3f84f2baabcde7b74469f058bb3e2b69c5335
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-13 22:02:39 +0100

    Update shipment table

--- migrations/000007_create_shipment_table.up.sql
+++ migrations/000007_create_shipment_table.up.sql
@@ -1,12 +1,11 @@
 CREATE TABLE IF NOT EXISTS shipment
 (
-    shipment_id   BIGSERIAL,
+    shipment_id   BIGSERIAL PRIMARY KEY,
     shipment_date TIMESTAMP(0) WITH TIME ZONE,
     address       TEXT         NOT NULL,
     city          VARCHAR(100) NOT NULL,
     state         VARCHAR(100) NOT NULL,
     country       VARCHAR(100) NOT NULL,
     zip_code      VARCHAR(10)  NOT NULL,
-    customer_id   BIGINT       NOT NULL REFERENCES customer (customer_id) ON DELETE CASCADE,
-    PRIMARY KEY (shipment_id, customer_id)
+    customer_id   BIGINT REFERENCES customer (customer_id) ON DELETE CASCADE
 );
