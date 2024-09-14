adaafc8de827fbf345258191970b723f0c42ccd7
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-14 17:28:13 +0100

    Update review table

--- migrations/000010_create_review_table.up.sql
+++ migrations/000010_create_review_table.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS review
 (
     review_id    BIGSERIAL PRIMARY KEY,
-    customer_id  BIGINT                      NOT NULL REFERENCES customer (customer_id),
-    furniture_id BIGINT                      NOT NULL REFERENCES furniture (furniture_id),
+    customer_id  BIGINT                      NOT NULL REFERENCES customer (customer_id) ON DELETE CASCADE,
+    furniture_id BIGINT                      NOT NULL REFERENCES furniture (furniture_id) ON DELETE CASCADE,
     rating       INTEGER                     NOT NULL,
     comment      TEXT                        NOT NULL,
     created_at   TIMESTAMP(0) WITH TIME ZONE NOT NULL,
