531ca8fbd0b03e41c4421501a8653f733a020ef2
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-15 12:28:45 +0100

    Add POST /v1/customers endpoint

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,6 +6,7 @@ func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	// Endpoints goes here
+	mux.HandleFunc("POST /v1/customers", app.registerCustomerHandler)
 
 	return mux
 }
