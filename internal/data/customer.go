44f5931b8b2a4730d952f8f8e2c7a047ffacde1b
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-16 15:30:53 +0100

    Remove Version field from customer_model.go struct

--- internal/data/customer.go
+++ internal/data/customer.go
@@ -17,7 +17,6 @@ type Customer struct {
 	Address     string    `json:"address"`
 	PhoneNumber string    `json:"phone_number"`
 	CreatedAt   time.Time `json:"created_at"`
-	Version     int       `json:"version"`
 }
 
 // password is a struct which contain the plaintext and hashed
@@ -58,7 +57,6 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	}
 	return true, nil
 }
-
 
 func ValidateEmail(v *validator.Validator, email string) {
 	v.Check(email != "", "email", "must be provided")
