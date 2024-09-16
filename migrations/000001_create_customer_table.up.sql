b09168bd27484af2ca72ebbe55b26d0ee79255ce
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-16 15:52:29 +0100

    Add role attribute to customer table

--- migrations/000001_create_customer_table.up.sql
+++ migrations/000001_create_customer_table.up.sql
@@ -6,5 +6,6 @@ CREATE TABLE IF NOT EXISTS customer
     password     BYTEA,
     address      TEXT,
     phone_number VARCHAR(100),
+    role         VARCHAR(15)                 NOT NULL DEFAULT 'customer',
     created_at   TIMESTAMP(0) WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
