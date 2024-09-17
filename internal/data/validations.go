7399ac8f8f80f53f6b8003e40db67150d9c403df
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-09-17 09:40:57 +0100

    Extract code related to validations in customer.go to validations.go

---
+++ internal/data/validations.go
@@ -0,0 +1,14 @@
+package data
+
+import "github.com/hayohtee/fumode/internal/validator"
+
+func ValidateEmail(v *validator.Validator, email string) {
+	v.Check(email != "", "email", "must be provided")
+	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
+}
+
+func ValidatePasswordPlainText(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
