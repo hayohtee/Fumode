57b0a822d2ad2d28b854e6719cba2a1ad55fcb11
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-14 13:14:58 +0100

    Update wishlist table to reference users

--- migrations/000004_create_wishlist_table.up.sql
+++ migrations/000004_create_wishlist_table.up.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS wishlist
     wishlist_id  BIGSERIAL,
     user_id  BIGINT NOT NULL REFERENCES users (user_id) ON DELETE CASCADE,
     furniture_id BIGINT NOT NULL REFERENCES furniture (furniture_id) ON DELETE CASCADE,
-    PRIMARY KEY (wishlist_id, customer_id)
+    PRIMARY KEY (wishlist_id, user_id)
 );
