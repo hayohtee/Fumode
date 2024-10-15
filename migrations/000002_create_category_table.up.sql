8f967feda13ce20215c3e96be892b9931c77b003
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-15 11:35:50 +0100

    Remove version attribute from the category table

--- migrations/000002_create_category_table.up.sql
+++ migrations/000002_create_category_table.up.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS category
 (
     category_id BIGSERIAL PRIMARY KEY,
-    name        VARCHAR(100) NOT NULL,
-    version     INTEGER      NOT NULL DEFAULT 1
+    name        VARCHAR(100) NOT NULL
 );
