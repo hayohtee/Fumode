a1911a0d2d272d774d07b2a109662b75c18f1a00
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-15 10:50:49 +0100

    Update routes.go

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -6,10 +6,10 @@ func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /v1/customers", app.registerCustomerHandler)
-	mux.HandleFunc("POST /v1/customers/login", app.loginCustomerHandler)
+	mux.HandleFunc("POST /v1/customers/login", app.loginUserHandler)
 
 	mux.HandleFunc("POST /v1/admins", app.registerAdminHandler)
-	mux.HandleFunc("POST /v1/admins/login", app.loginAdminHandler)
+	mux.HandleFunc("POST /v1/admins/login", app.loginUserHandler)
 
 	return mux
 }
