15df856da7a186f89ebda70b807b31225f675717
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-15 10:05:23 +0100

    Add users.go

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -1,1 +1,147 @@
 package main
+
+import (
+	"errors"
+	"fmt"
+	"github.com/hayohtee/fumode/internal/data"
+	"github.com/hayohtee/fumode/internal/validator"
+	"net/http"
+	"time"
+)
+
+func (app *application) registerCustomerHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name     string `json:"name"`
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	user := data.User{
+		Name:  input.Name,
+		Email: input.Email,
+		Role:  CustomerRole,
+	}
+
+	err = user.Password.Set(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+	if data.ValidateUser(v, user); !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	err = app.repositories.Users.Insert(&user)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrDuplicateEmail):
+			v.AddError("email", "a user with this email already exists")
+			app.failedValidationResponse(w, r, v.Errors)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	// Launch a goroutine to send welcome email
+	app.background(func() {
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl", user)
+		if err != nil {
+			app.serverErrorResponse(w, r, err)
+			return
+		}
+	})
+
+	response := UserResponse{
+		ID:        user.UserID,
+		Name:      user.Name,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt,
+		Role:      user.Role,
+	}
+
+	err = app.writeJSON(w, http.StatusCreated, envelope{"customer": response}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+func (app *application) loginCustomerHandler(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	err := app.readJSON(w, r, &input)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	v := validator.New()
+	data.ValidateEmail(v, input.Email)
+	data.ValidatePasswordPlainText(v, input.Password)
+	if !v.Valid() {
+		app.failedValidationResponse(w, r, v.Errors)
+		return
+	}
+
+	user, err := app.repositories.Users.GetByEmail(input.Email)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.errorResponse(w, r, http.StatusNotFound, "the provided email address could not be found")
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+
+	match, err := user.Password.Matches(input.Password)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	if !match {
+		app.unauthorizedResponse(w, r, "invalid credentials. Please check your email and password")
+		return
+	}
+
+	token, err := generateJWT(user.UserID, user.Role)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	response := UserResponse{
+		ID:        user.UserID,
+		Name:      user.Name,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt,
+		Role:      user.Role,
+	}
+
+	w.Header().Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	err = app.writeJSON(w, http.StatusOK, envelope{"customer": response}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
+
+type UserResponse struct {
+	ID        int64     `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+	Role      string    `json:"role"`
+}
