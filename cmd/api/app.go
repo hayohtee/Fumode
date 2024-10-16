e44c8689e03f79b214a4100a4b4dfb00f828a816
Olamilekan Akintilebo <olamilekanakintilebo@gmail.com>
2024-10-16 10:25:48 +0100

    Rename config to configuration

--- cmd/api/app.go
+++ cmd/api/app.go
@@ -5,15 +5,17 @@ import (
 	"github.com/hayohtee/fumode/internal/mailer"
 	"sync"
 
+	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/hayohtee/fumode/internal/jsonlog"
 )
 
 // application holds the dependencies for the handlers, middlewares
 // and helpers.
 type application struct {
-	config       config
+	config       configuration
 	logger       *jsonlog.Logger
 	wg           sync.WaitGroup
 	repositories data.Repositories
 	mailer       mailer.Mailer
+	s3Client     *s3.Client
 }
